8cebbdaeaadf1e11e6f43b283a5ec244320c2af2
IneptDwarf <IneptDwarf33872@gmail.com>
2025-05-27 07:02:59 -0400

    docker stuff

--- server.js
+++ server.js
@@ -15,18 +15,16 @@ const { exec, spawn } = require('node:child_process');
 let occupied = false;
 
 app.get('/is-occupied', (req, res) => {
-    res.send(occupied+"")
+    res.send(occupied + "")
 })
-
-
 
 wss.on('connection', (ws) => {
 
     console.log('A client attempted to connect');
-    if(occupied){
+    if (occupied) {
         console.log("Denied join request (Container full)")
         ws.close(1001, 'Container Occupied')
-    }else{
+    } else {
         console.log("Accepted join request")
         ws.send('Join Success');
         occupied = true;
@@ -37,19 +35,19 @@ wss.on('connection', (ws) => {
 
         msg = msg.toString();
         let hyphenIndex = -1;
-        for(let i = 0; i < msg.length; i++){
-            if(msg[i] === "-"){
+        for (let i = 0; i < msg.length; i++) {
+            if (msg[i] === "-") {
                 hyphenIndex = i;
                 break;
             }
         }
 
         const type = msg.substring(0, hyphenIndex);
-        const code = msg.substring(hyphenIndex+1);
-        console.log("Language: "+type);
+        const code = msg.substring(hyphenIndex + 1);
+        console.log("Language: " + type);
         console.log("Code: " + code);
 
-        if(type === 'java'){
+        if (type === 'java') {
             fs.writeFile('Main.java', code, (err) => {
                 if (err) {
                     console.log("File Write Failed");
@@ -63,24 +61,30 @@ wss.on('connection', (ws) => {
                     if (err) {
                         console.log("Failed to echo file: " + err);
                         ws.close(3420, "Failed to echo file");
-                    }else if (stderr){
+                    } else if (stderr) {
                         console.log("Failed to echo file: " + stderr);
                         ws.close(3420, "Failed to echo file");
-                    }else if(stdout){
+                    } else if (stdout) {
                         exec('javac Main.java', (err, stdout, stderr) => {
                             if (err) {
                                 console.log("Compilation Error:");
                                 console.log(err);
                                 ws.send(err.message);
                                 ws.close(3420, "Compilation Error");
-                            }else if (stderr){
+                            } else if (stderr) {
                                 console.log("Compilation Error:");
                                 console.log(stderr);
                                 ws.send(stderr.message);
                                 ws.close(3420, "Compilation Error");
-                            }else{
+                            } else {
                                 const dockerProcess = spawn('java', ['Main']);
-                                
+
+                                const timeout = setTimeout(() => {
+                                    dockerProcess.kill('SIGKILL');
+                                    ws.send("Process timed out after 10 seconds.");
+                                    ws.close(3420, "Execution Timeout");
+                                }, 10000);
+
                                 dockerProcess.stdout.on('data', (data) => {
                                     console.log(data.toString());
                                     ws.send(data);
@@ -90,11 +94,10 @@ wss.on('connection', (ws) => {
                                     ws.send(data);
                                 });
                                 dockerProcess.on('close', (code) => {
-                                    dockerProcess.kill()
+                                    clearTimeout(timeout);
                                     setTimeout(() => {
                                         ws.close(3420, `Exited with code ${code}`);
-                                    }, 500)
-                                    
+                                    }, 500);
                                 });
                             }
                             return;
@@ -102,9 +105,9 @@ wss.on('connection', (ws) => {
                     }
                     return;
                 });
-             });
+            });
 
-        }else if(type === 'python'){
+        } else if (type === 'python') {
 
             fs.writeFile('PyRunner.py', code, (err) => {
                 if (err) {
@@ -121,71 +124,68 @@ wss.on('connection', (ws) => {
                         console.log(err);
                         ws.send(err.message);
                         ws.close(3420, "Compilation Error");
-                    }else if (stderr){
+                    } else if (stderr) {
                         console.log("Compilation Error:");
                         console.log(stderr);
                         ws.send(stderr.message);
                         ws.close(3420, "Compilation Error");
-                    }else{
+                    } else {
                         const dockerProcess = spawn('python3', ['./PyRunner.py']);
 
+                        const timeout = setTimeout(() => {
+                            dockerProcess.kill('SIGKILL');
+                            ws.send("Process timed out after 10 seconds.");
+                            ws.close(3420, "Execution Timeout");
+                        }, 10000);
+
                         dockerProcess.stdout.on('data', (data) => {
-                                console.log(data.toString());
-                                ws.send(data);
-                             });
-                            dockerProcess.stderr.on('data', (data) => {
-                                console.log(data.toString());
-                                ws.send(data);
-                            });
-                            dockerProcess.on('close', (code) => {
-                                ws.close(3420, `Exited with code ${code}`);
-                            });
-                            
+                            console.log(data.toString());
+                            ws.send(data);
+                        });
+                        dockerProcess.stderr.on('data', (data) => {
+                            console.log(data.toString());
+                            ws.send(data);
+                        });
+                        dockerProcess.on('close', (code) => {
+                            clearTimeout(timeout);
+                            ws.close(3420, `Exited with code ${code}`);
+                        });
+
                         return;
-                        
+
                     }
                     return;
                 });
-             });
+            });
 
-
-
-
-            
         }
-        
+
     });
-  
+
     ws.on('close', () => {
         occupied = false;
-      console.log('Client disconnected');
+        console.log('Client disconnected');
     });
 
-  });
+});
 
-  server.listen(3080, '0.0.0.0', () => {
+server.listen(3080, '0.0.0.0', () => {
     console.log('Server is listening on port 3080');
     attempt();
-  });
+});
 
+async function attempt() {
+    console.log("Attempting at " + 'http://' + serverIP + ':3000/register-runner' + "...");
+    try {
+        await fetch('http://' + serverIP + ':3000/register-runner')
+            .then(res => {
+                return;
+            })
+            .catch(error => {
+                setTimeout(attempt, 10000)
+            })
 
-  async function attempt() {
-    console.log("Attempting at " + 'http://'+serverIP+':3000/register-runner' + "...");
-    try{
-        await fetch('http://'+serverIP+':3000/register-runner')
-        .then(res =>{
-            return;
-        })
-        .catch(error => {
-            setTimeout(attempt, 10000)
-        })
-
-    } catch(error){
+    } catch (error) {
         setTimeout(attempt, 10000)
     }
 }
-
-
-
-
-
